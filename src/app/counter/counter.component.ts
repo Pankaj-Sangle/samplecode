4d66fe98674f8231e4fb2cfa1a2afc04df47a77c
Prasad Nalawade <nalawadeprasad2001@gmail.com>
2023-03-18 11:25:57 +0530

    local changes

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -5,4 +5,43 @@ import { Component } from "@angular/core";
     templateUrl: './counter.component.html',
     styleUrls: ['./counter.component.css']
 })
-export class CounterComponent {}
+export class CounterComponent {
+    products = [
+        {
+            id: 1,
+            count: 0,
+            productImg: 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        },
+        {
+            id: 2,
+            count: 0,
+            productImg: 'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+        },
+        {
+            id: 3,
+            count: 0,
+            productImg: 'https://images.unsplash.com/photo-1614165936126-2ed18e471b3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+        },
+        {
+            id: 4,
+            count: 0,
+            productImg: 'https://images.unsplash.com/photo-1559678478-1839b04e292b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+        }
+    ];
+
+    handleDecrement(id: number) {
+        const product = this.products.find((product) => product.id === id);
+
+        if (product && product.count) {
+            product.count = product.count - 1;
+        }
+    }
+
+    handleIncrement(id: number) {
+        const product = this.products.find((product) => product.id === id);
+
+        if (product) {
+            product.count = product.count + 1;
+        }
+    }
+}
--- src/app/counter/product/product.component.ts
+++ src/app/counter/product/product.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+interface ProductInfo {
+  id: number;
+  count: number;
+  productImg: string;
+}
 
 @Component({
   selector: 'cnt-product',
@@ -7,18 +13,30 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
-  count: number = 0;
-  productImg: string = 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+
+  @Input()
+  productInfo!: ProductInfo;
+
+  @Output()
+  decrementEvent: EventEmitter<any> = new EventEmitter();
+  
+  @Output()
+  incrementEvent: EventEmitter<any> = new EventEmitter();
+
+  // count: number = 0;
+  // productImg: string = 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
 
   handleDecrement(): void {
-    if (this.count) {
-      this.count = this.count - 1;
-    }
+    this.decrementEvent.emit(this.productInfo.id);
+    // if (this.count) {
+    //   this.count = this.count - 1;
+    // }
     
   }
 
   handleIncrement(): void {
-    this.count = this.count + 1;
+    this.incrementEvent.emit(this.productInfo.id);
+    // this.count = this.count + 1;
   }
 
   handleDelete(): void {
